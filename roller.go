7473ce8020c3ab5b5da89559d3de987fcba0b5d9
Shane Liebling <shanel@gmail.com>
2017-07-26 09:11:45 -0400

    some more tweaks to get labels slightly less janky

--- roller.go
+++ roller.go
@@ -543,6 +543,7 @@ func about(w http.ResponseWriter, r *http.Request) {
 	      <p>This is a dice roller.</p>
 	      <p>Give the URL for the room to others and they can see and do everything you can see and do.</p>
 	      <p>It was inspired by (and uses the dice images from) <a href="https://catchyourhare.com/diceroller/">this dice roller.</a></p>
+	      <p>The title comes from an idea who's authorship is discussed <a href="http://story-games.com/forums/discussion/comment/276305/#Comment_276305">here</a>.</p>
 	      <p>The token image is "coin by Arthur Shlain from the Noun Project."</p>
 	      <p>The code is available <a href="https://github.com/shanel/roller">here</a>.</p>
 	      <p>Bugs or feature requests should go <a href="https://github.com/shanel/roller/issues">here</a>.</p>
@@ -601,7 +602,11 @@ func label(w http.ResponseWriter, r *http.Request) {
 	}
 	// Initialize the context.
 	fg, bg := image.NewUniform(Convert(cols[col])), image.Black
-	width := (((utf8.RuneCountInString(text) * int(18)) / 72) * 58) + 30
+	rc := utf8.RuneCountInString(text)
+	if (rc % 2) == 0 {
+		rc += 1
+	}
+	width := (int(math.Ceil((float64(rc) * float64(18)) / float64(72))) * 52)// + 10
 	rgba := image.NewRGBA(image.Rect(0, 0, width, 48))
 	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
 	fc := freetype.NewContext()
@@ -634,10 +639,11 @@ var roomTemplate = template.Must(template.New("room").Parse(`
 
 <head>
     <title>Roll Dice Or Say Yes</title>
-    <link type="text/css" rel="stylesheet" href="/css/drag.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/interact.js/1.2.9/interact.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/1.5.1/fingerprint2.min.js"></script>
+
+<!--    <a href="https://your-url" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#fff; color:#151513; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style> -->
 
     <a href="https://github.com/shanel/roller" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
 
@@ -807,19 +813,36 @@ var roomTemplate = template.Must(template.New("room").Parse(`
     </script>
 </head>
 
+<style type="text/css">
+body {
+	color:#000000;
+	background-color:#ffffff;
+}
+
+.tap-target {
+  display: inline-block;
+}
+
+.tap-target.to-delete {
+  background-color: #f40;
+  border-style: solid;
+  border-color: red;
+}
+</style>
+
 <body>
     <center>
-        <h1><a href="http://story-games.com/forums/discussion/comment/276305/#Comment_276305">Roll Dice Or Say Yes</a></h1>
+        <h1>Roll Dice Or Say Yes</h1>
         <form action="/roll" method="post">
-            <input type="text" name="d4" style="width: 25px"></input>d4
-            <input type="text" name="d6" style="width: 25px"></input>d6
-            <input type="text" name="d8" style="width: 25px"></input>d8
-            <input type="text" name="d10" style="width: 25px"></input>d10
-            <input type="text" name="d12" style="width: 25px"></input>d12
-            <input type="text" name="d20" style="width: 25px"></input>d20
-            <input type="text" name="dF" style="width: 25px"></input>dF
-            <input type="text" name="tokens" style="width: 25px"></input>tokens
-            <input type="text" name="label" style="width: 100"></input>label
+            <input type="text" name="d4" style="width: 25px"></input> d4
+            <input type="text" name="d6" style="width: 25px"></input> d6
+            <input type="text" name="d8" style="width: 25px"></input> d8
+            <input type="text" name="d10" style="width: 25px"></input> d10
+            <input type="text" name="d12" style="width: 25px"></input> d12
+            <input type="text" name="d20" style="width: 25px"></input> d20
+            <input type="text" name="dF" style="width: 25px"></input> dF
+            <input type="text" name="tokens" style="width: 25px"></input> tokens
+            <input type="text" name="label" style="width: 100"></input> label
 
             <select id="selectColor" name="color">
 			<option value="blue" style="color: #1e90ff">Blue</option>
