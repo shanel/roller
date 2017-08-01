0658d17b80447925468c73314814cc8cb9c943cb
Shane Liebling <shanel@gmail.com>
2017-08-01 08:36:25 -0400

    tooltip re pipped d6

--- roller.go
+++ roller.go
@@ -1027,6 +1027,33 @@ button {
 
 .button2 { background-color: #f44336; color: white; }
 
+/* Tooltip container */
+.tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+}
+
+/* Tooltip text */
+.tooltip .tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+ 
+    /* Position the tooltip text - see examples below! */
+    position: absolute;
+    z-index: 1;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip:hover .tooltiptext {
+    visibility: visible;
+}
+
 </style>
 
 <body>
@@ -1036,7 +1063,7 @@ button {
         <form id="rollem" action="/roll" method="post">
             <input type="text" name="d4" style="width: 19px"></input> d4
             <input type="text" name="d6" style="width: 19px"></input> d6
-            <input type="text" name="d6p" style="width: 19px"></input> d6(P)
+            <input type="text" name="d6p" style="width: 19px"></input> <div class="tooltip">d6(P)<span class="tooltiptext">d6 with pips</span></div>
             <input type="text" name="d8" style="width: 19px"></input> d8
             <input type="text" name="d10" style="width: 19px"></input> d10
             <input type="text" name="d12" style="width: 19px"></input> d12
