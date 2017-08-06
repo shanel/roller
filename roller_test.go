8c0f787f445827cdae021a8b1fae7f6ebc2de8dc
Shane Liebling <shanel@gmail.com>
2017-08-06 12:35:10 -0400

    initial test

--- roller_test.go
+++ roller_test.go
@@ -2,9 +2,7 @@ package roller
 
 import (
 	"testing"
-	
-	//"golang.org/x/net/context"
-	//"google.golang.org/appengine"
+
 	"google.golang.org/appengine/aetest"
 	"google.golang.org/appengine/datastore"
 )
@@ -15,11 +13,20 @@ func TestGetEncodedRoomKeyFromName(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer done()
+
+	got, err := getEncodedRoomKeyFromName(ctx, "HappyFunBall")
+	if err == nil {
+		t.Fatalf("getEncodedRoomFromName(ctx, 'HappyFunBall') == %v, %v; want _, err", got, err)
+	}
 	key := datastore.NewKey(ctx, "Room", "", 1, nil)
 	if _, err := datastore.Put(ctx, key, &Room{Slug: "HappyFunBall"}); err != nil {
 		t.Fatal(err)
 	}
-	got, err := getEncodedRoomKeyFromName(ctx, "HappyFunBall")
+	var r Room
+	if err = datastore.Get(ctx, key, &r); err != nil {
+		t.Fatal(err)
+	}
+	got, err = getEncodedRoomKeyFromName(ctx, "HappyFunBall")
 	if err != nil || got != key.Encode() {
 		t.Fatalf("getEncodedRoomFromName(ctx, 'HappyFunBall') == %v, %v; want %v, nil", got, err, key.Encode())
 	}
